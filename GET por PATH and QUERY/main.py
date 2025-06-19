544cad9e02d0418ee9b61e42a84380c6784cb197
GabrielHome <lopezalvaradoa749@gmail.com>
2025-06-19 09:41:53 -0600

    Realizado dos get queries

--- GET por PATH and QUERY/main.py
+++ GET por PATH and QUERY/main.py
@@ -2,6 +2,7 @@
 import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import Optional
 
 # Creamos una instancia de FastAPI
 app = FastAPI()
@@ -9,16 +10,16 @@ app = FastAPI()
 # Definimos nuestro entidad clase que se usará como ejemplo
 
 class User(BaseModel):
-    nombre_completo:str
-    matricula: int
-    edad: int
-    carrera: str
-    genero: str
-    semestre: int
-    porcentaje_carrera: float
-    facultad: str
-    materias_reprobadas: int
-    promedio: float
+    nombre_completo: Optional[str] = None
+    matricula: Optional[int] = None
+    edad: Optional[int] = None
+    carrera: Optional[str] = None
+    genero: Optional[str] = None
+    semestre: Optional[int] = None
+    porcentaje_carrera: Optional[float] = None
+    facultad: Optional[str] = None
+    materias_reprobadas: Optional[int] = None
+    promedio: Optional[float] = None
 
 def estudiantes(file_path: str) -> list[User]:
     df = pd.read_excel(file_path)
@@ -158,102 +159,43 @@ async def userclass(prom:float):
         return list(users)
     except:
         return{"No se ha encontrado al usuario"}
-# # Path con un solo nivel
-
-# # Primero
-# @app.get("/estudiantes")
-# async def students():
-#     file_path = "Registros.xlsx"
-#     users = estudiantes(file_path)
-#     return {"Alumnos registrados": users}
-
-# # Segundo
-# @app.get("/carreras")
-# async def carreras():
-#     file_path = "Registros.xlsx"
-#     df = pd.read_excel(file_path)
-#     carreras_unicas = df["Carrera"].unique().tolist()
-
-#     return {"Carreras": carreras_unicas}
-
-# # Tercero
-# @app.get("/generos")
-# async def generos():
-#     file_path = "Registros.xlsx"
-#     df = pd.read_excel(file_path)
-#     carreras_unicas = df["Genero"].unique().tolist()
-
-#     return {"Generos": carreras_unicas}
-
-# # Cuarto
-# @app.get("/semestres")
-# async def semestres():
-#     file_path = "Registros.xlsx"
-#     df = pd.read_excel(file_path)
-#     carreras_unicas = sorted(df["Semestre"].unique().tolist())
-
-#     return {"Semestres": carreras_unicas}
+    
 
 
-# # Path con solo dos niveles
-
-# # Primero
-# @app.get("/estudiantes/mujeres")
-# async def estudMujeres():
-#     file_path = "Registros.xlsx"
-#     df = pd.read_excel(file_path)
-#     estud_mujeres = df[df["Genero"] == "FEMENINO"]
-
-#     return  estud_mujeres.to_dict(orient="records")
-
-
-# # Segundo
-# @app.get("/estudiantes/hombres")
-# async def estudHombres():
-#     file_path = "Registros.xlsx"
-#     df = pd.read_excel(file_path)
-#     estud_hombres = df[df["Genero"] == "MASCULINO"]
-
-#     return  estud_hombres.to_dict(orient="records")
+"""GET por QUERY"""
+"""NOMBRE y EDAD"""
+@app.get("/usersclass2/")
+async def usersclass(age:int, name: str):
+    file_path = "Registros.xlsx"
+    users = estudiantes(file_path)
+    users = filter (lambda user: user.edad == age and user.nombre_completo == name,users)
+    try:
+        return list(users)
+    except:
+        return{"No se ha encontrado al usuario"}
+"""http://127.0.0.1:8000/usersclass2/?age=21&name=LOPEZ%20ALVARADO%20ANGEL%20GABRIEL"""
 
 
-# # Tercero
-# @app.get("/estudiantes/veinteaños")
-# async def estudVeinteAños():
-#     file_path = "Registros.xlsx"
-#     df = pd.read_excel(file_path)
-#     edad_veinte = df[df["Edad"] == 20]
-
-#     return  edad_veinte.to_dict(orient="records")
-
-
-# # Path con solo tres niveles
-
-# # Primero
-# @app.get("/estudiantes/ITI/promedio")
-# async def estudITIProm():
-#     file_path = "Registros.xlsx"
-#     df = pd.read_excel(file_path)
-#     carrera = df[df["Carrera"] == "ITI"]
-#     promedio = carrera[["Nombre Completo","Carrera","Promedio"]]
-#     return  promedio.to_dict(orient="records")
+"""MATRICULA"""
+@app.get("/usersclass3/")
+async def usersclass(matricula:int):
+    file_path = "Registros.xlsx"
+    users = estudiantes(file_path)
+    users = filter (lambda user: user.matricula == matricula,users)
+    try:
+        return list(users)
+    except:
+        return{"No se ha encontrado al usuario"}
+"""http://127.0.0.1:8000/usersclass3/?matricula=202108321"""
 
 
-# # Segundo
-# @app.get("/estudiantes/masculinos/octavosemestre")
-# async def estudMascOctaSem():
-#     file_path = "Registros.xlsx"
-#     df = pd.read_excel(file_path)
-#     gen_sem = df[(df["Genero"] == "MASCULINO") & (df["Semestre"] == 8)]
-#     promedio = gen_sem[["Nombre Completo","Genero","Semestre"]]
-#     return  promedio.to_dict(orient="records")
-
-
-# # Tercero
-# @app.get("/estudiantes/ICC/porcentajecarrera")
-# async def estudICCPorcCarrera():
-#     file_path = "Registros.xlsx"
-#     df = pd.read_excel(file_path)
-#     carrera = df[df["Carrera"] == "ICC"]
-#     procentaje = carrera[["Nombre Completo","Carrera","Porcentaje de carrera"]]
-#     return  procentaje.to_dict(orient="records")
+"""CARRERA y EDAD"""
+@app.get("/usersclass4/")
+async def usersclass(matricula:int):
+    file_path = "Registros.xlsx"
+    users = estudiantes(file_path)
+    users = filter (lambda user: user.matricula == matricula,users)
+    try:
+        return list(users)
+    except:
+        return{"No se ha encontrado al usuario"}
